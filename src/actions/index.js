76f27842412e8590161fc5f68348b345724c0108
Jaba <jabaqadagishvili4@gmail.com>
2024-01-09 06:40:23 +0400

    initial commit / already added functional to remove heroes from the global state

---
+++ src/actions/index.js
@@ -0,0 +1,25 @@
+export const heroesFetching = () => {
+    return {
+        type: 'HEROES_FETCHING'
+    }
+}
+
+export const heroesFetched = (heroes) => {
+    return {
+        type: 'HEROES_FETCHED',
+        payload: heroes
+    }
+}
+
+export const heroesFetchingError = () => {
+    return {
+        type: 'HEROES_FETCHING_ERROR'
+    }
+}
+
+export const onDelete = (index) => {
+    return {
+        type: 'DELETED_HERO',
+        payload: index
+    }
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,36 @@
+const initialState = {
+    heroes: [],
+    heroesLoadingStatus: 'idle',
+    filters: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'HEROES_FETCHING':
+            return {
+                ...state,
+                heroesLoadingStatus: 'loading'
+            }
+        case 'HEROES_FETCHED':
+            return {
+                ...state,
+                heroes: action.payload,
+                heroesLoadingStatus: 'idle'
+            }
+        case 'HEROES_FETCHING_ERROR':
+            return {
+                ...state,
+                heroesLoadingStatus: 'error'
+            }
+        case 'DELETED_HERO':
+            const newHeroList = [...state.heroes]
+            newHeroList.splice(action.payload, 1)
+            return {
+                ...state,
+                heroes: newHeroList
+            }
+        default: return state
+    }
+}
+
+export default reducer;
